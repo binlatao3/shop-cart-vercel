eab77dccc0360f4edcc782195f59627a4e584989
unknown <binlatao3@gmail.com>
2023-03-26 18:06:00 +0700

    deploy to vercel

--- db.js
+++ db.js
@@ -5,11 +5,7 @@ const app = express()
 
 app.set('env', 'development')
 
-switch(app.get('env')){
-    case 'development':
-        mongoose.connect(process.env.MONGODB_URI,{
-            useNewUrlParser: true,
-        })
-        console.log(`Connect db success - ${app.get('env')}`)
-        break
-}
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+})
+console.log(`Connect db success`)
