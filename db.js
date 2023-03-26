7e9ad40a73be71f5aa3b3500ad5d73d45ed03822
unknown <binlatao3@gmail.com>
2023-03-26 20:23:54 +0700

    deploy to vercel

--- db.js
+++ db.js
@@ -3,7 +3,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true,
-})
-console.log(`Connect db success`)
+if(MONGODB_URI)
+{
+    mongoose.connect(process.env.MONGODB_URI,{
+        useNewUrlParser: true,
+    })
+    console.log(`Connect db success to production`)
+}
+else
+{
+    mongoose.connect("mongodb://127.0.0.1:27017/ShopCart?readPreference=primary&directConnection=true&ssl=false",{
+        useNewUrlParser: true,
+    })
+    console.log(`Connect db success to development`)
+}
