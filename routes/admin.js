7883017e973b51a35fd7b7826497e1a7c04a681f
unknown <binlatao3@gmail.com>
2023-03-26 22:41:26 +0700

    deploy to vercel

--- routes/admin.js
+++ routes/admin.js
@@ -328,7 +328,7 @@ router.post('/add-product',uploader.fields([{name:'myImage'}]),addValidator,(req
             }
         })
         product.save().then(()=>{
-            fs.renameSync(myImage[0].path,newPathImage + myImage[0].originalname)
+            fs.renameSync(newPathImage + myImage[0].filename,newPathImage + myImage[0].originalname)
             console.log('Susscess')
             res.redirect('list-product')
         }).catch((err)=>{
